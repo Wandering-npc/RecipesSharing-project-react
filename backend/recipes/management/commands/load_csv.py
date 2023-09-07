a31bb4ef783f88e19fbe815aeca7149964f5264b
WorkPC <Los-Shepardos@yandex.ru>
2023-09-07 10:29:56 +0300

    review4

---
+++ backend/recipes/management/commands/load_csv.py
@@ -0,0 +1,23 @@
+import csv
+import os
+from django.conf import settings
+
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+DATA_ROOT = os.path.join(settings.BASE_DIR, 'data')
+
+class Command(BaseCommand):
+    help = '''Загрузка ингредиентов в базу.'''
+
+    def handle(self, *args, **options):
+        with open(os.path.join(DATA_ROOT, options['filename']),
+                      'r', encoding='utf-8') as file:
+            reader_data = csv.reader(file)
+            for row in reader_data:
+                name, measurement_unit = row
+                Ingredient.objects.get_or_create(
+                    name=name,
+                    measurement_unit=measurement_unit
+                )
