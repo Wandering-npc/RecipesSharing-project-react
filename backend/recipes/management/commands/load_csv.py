ccdbeaa5dbecc5875dc3387e5f16006a28fe8893
WorkPC <Los-Shepardos@yandex.ru>
2023-09-11 15:28:31 +0300

    dasdas

--- backend/recipes/management/commands/load_csv.py
+++ backend/recipes/management/commands/load_csv.py
@@ -7,15 +7,12 @@ from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
 
 
-DATA_ROOT = os.path.join(settings.BASE_DIR, 'data')
-
-
 class Command(BaseCommand):
     help = 'Загрузка ингредиентов в базу.'
 
     def handle(self, *args, **options):
-        with open(os.path.join(DATA_ROOT, options['filename']),
-                  'r', encoding='utf-8') as file:
+        path = os.path.join(settings.BASE_DIR, 'ingredients.csv')
+        with open(path, 'r', encoding='utf-8') as file:
             reader_data = csv.reader(file)
             for name, measurement_unit in reader_data:
                 Ingredient.objects.get_or_create(
