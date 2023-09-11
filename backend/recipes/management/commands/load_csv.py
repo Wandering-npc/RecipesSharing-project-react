fbde7859f7586fa4a1f3c996d3673372a2022e7b
WorkPC <Los-Shepardos@yandex.ru>
2023-09-11 16:00:12 +0300

    review7

--- backend/recipes/management/commands/load_csv.py
+++ backend/recipes/management/commands/load_csv.py
@@ -8,11 +8,11 @@ from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = 'Загрузка ингредиентов в базу.'
+    help = "Загрузка ингредиентов в базу."
 
     def handle(self, *args, **options):
-        path = os.path.join(settings.BASE_DIR, 'ingredients.csv')
-        with open(path, 'r', encoding='utf-8') as file:
+        path = os.path.join(settings.BASE_DIR, "ingredients.csv")
+        with open(path, "r", encoding="utf-8") as file:
             reader_data = csv.reader(file)
             for name, measurement_unit in reader_data:
                 Ingredient.objects.get_or_create(
